0af1a0c232f8efed2a2253a814d795e3d5c50648
Patryk Sosinski <sosiniak95@gmail.com>
2021-03-01 17:22:30 +0100

    Add Spring init classes

---
+++ src/main/java/pl/sosinski/patryk/letsmeet/LetsMeetApplication.java
@@ -0,0 +1,13 @@
+package pl.sosinski.patryk.letsmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsMeetApplication.class, args);
+	}
+
+}

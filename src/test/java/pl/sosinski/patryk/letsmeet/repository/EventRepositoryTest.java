ff2381882eae7da9befdc19a6fc6f7afada2ee8a
Vicardo1 <vicardo95@gmail.com>
2021-04-12 17:26:45 +0200

    Add Participants to EventEntity

--- src/test/java/pl/sosinski/patryk/letsmeet/repository/EventRepositoryTest.java
+++ src/test/java/pl/sosinski/patryk/letsmeet/repository/EventRepositoryTest.java
@@ -3,18 +3,21 @@ package pl.sosinski.patryk.letsmeet.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
 import pl.sosinski.patryk.letsmeet.repository.entity.EventEntity;
 import pl.sosinski.patryk.letsmeet.repository.entity.ParticipantEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class EventRepositoryTest {
 
     public static final String EVENT_ENTITY_NAME_JAVA = "Szkolenie Java";
     public static final String PARTICIPANT_FIRST_NAME_PATRYK = "Patryk";
+    public static final int EVENT_PARTICIPANTS_SIZE_2 = 2;
 
     @Autowired
     private EventRepository eventRepository;
@@ -62,6 +65,34 @@ class EventRepositoryTest {
                 () -> assertNotNull(savedEventEntity, "EventEntity is null"),
                 () -> assertNotNull(savedEventEntity.getId(), "EventEntity.id is null")
         );
+    }
+
+    @Test
+    void given_when_then2() {
+        //Given
+        EventEntity eventEntity = new EventEntity();
+        HashSet<ParticipantEntity> participants = new HashSet<>();
+        ParticipantEntity participantEntity1 = new ParticipantEntity();
+        ParticipantEntity participantEntity2 = new ParticipantEntity();
+        eventEntity.setHost(participantEntity1);
+        participants.add(participantEntity1);
+        participants.add(participantEntity2);
+
+        //When
+        participantRepository.save(participantEntity1);
+        participantRepository.save(participantEntity2);
+
+        eventEntity.setParticipants(participants);
+        EventEntity savedEventEntity = eventRepository.save(eventEntity);
+
+        //Then
+        assertAll(
+                () -> assertNotNull(savedEventEntity, "EventEntity is null"),
+                () -> assertNotNull(savedEventEntity.getHost(), "EventEntity hasn't a host"),
+                () -> assertEquals(savedEventEntity.getParticipants().size(), EVENT_PARTICIPANTS_SIZE_2,
+                        "Participants isn't equal to " + EVENT_PARTICIPANTS_SIZE_2)
+        );
+
     }
 
 }

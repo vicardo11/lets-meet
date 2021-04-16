14e4b11548681285a18367ba4731c91e736c433b
Vicardo1 <vicardo95@gmail.com>
2021-04-16 12:04:23 +0200

    Add InterestController with tests

--- src/main/java/pl/sosinski/patryk/letsmeet/web/controller/rest/ControllerConstants.java
+++ src/main/java/pl/sosinski/patryk/letsmeet/web/controller/rest/ControllerConstants.java
@@ -4,4 +4,5 @@ public class ControllerConstants {
 
     public static final String NOTIFICATIONS_URI = "/notifications";
     public static final String EVENTS_URI = "/events";
+    public static final String INTEREST_URI = "/interest";
 }
---
+++ src/test/java/pl/sosinski/patryk/letsmeet/web/controller/rest/InterestControllerTest.java
@@ -0,0 +1,91 @@
+package pl.sosinski.patryk.letsmeet.web.controller.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.sosinski.patryk.letsmeet.service.InterestService;
+import pl.sosinski.patryk.letsmeet.web.model.InterestModel;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pl.sosinski.patryk.letsmeet.web.controller.rest.ControllerConstants.INTEREST_URI;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class InterestControllerTest {
+
+    public static final long INTEREST_ID_1 = 1L;
+    public static final String INTEREST_NAME_SPORT = "Sport";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private InterestService interestService;
+
+    @Test
+    void givenInterestUriAndInterestId_whenGet_thenStatusIsOk() throws Exception {
+        //Given
+
+        //When
+        mockMvc.perform(get(INTEREST_URI + "/" + INTEREST_ID_1))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //Then
+    }
+
+    @Test
+    void givenInterestUri_whenGet_thenStatusIsOk() throws Exception {
+        //Given
+
+        //When
+        mockMvc.perform(get(INTEREST_URI))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //Then
+    }
+
+    @Test
+    void givenInterestUri_whenPost_thenStatusIsOk() throws Exception {
+        //Given
+        InterestModel interestModel = new InterestModel();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String valueAsString = objectMapper.writeValueAsString(interestModel);
+
+        //When
+        mockMvc.perform(post(INTEREST_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //Then
+    }
+
+    @Test
+    void givenInterestUri_whenPut_thenStatusIsOk() throws Exception {
+        //Given
+        InterestModel interestModel = InterestModel.builder()
+                .id(INTEREST_ID_1)
+                .name(INTEREST_NAME_SPORT)
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String interestModelAsString = objectMapper.writeValueAsString(interestModel);
+
+        //When
+        mockMvc.perform(put(INTEREST_URI + "/" + INTEREST_ID_1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(interestModelAsString))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //Then
+    }
+}

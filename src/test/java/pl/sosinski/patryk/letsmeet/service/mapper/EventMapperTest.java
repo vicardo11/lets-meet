c7524d49aac438f6a8ad24ed88b9c6b42bd072f9
Vicardo1 <vicardo95@gmail.com>
2021-05-08 11:15:27 +0200

    Additional tests for EventMapper

--- src/test/java/pl/sosinski/patryk/letsmeet/service/mapper/EventMapperTest.java
+++ src/test/java/pl/sosinski/patryk/letsmeet/service/mapper/EventMapperTest.java
@@ -1,13 +1,15 @@
 package pl.sosinski.patryk.letsmeet.service.mapper;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pl.sosinski.patryk.letsmeet.repository.entity.EventEntity;
 import pl.sosinski.patryk.letsmeet.repository.entity.ParticipantEntity;
 import pl.sosinski.patryk.letsmeet.web.model.EventModel;
 import pl.sosinski.patryk.letsmeet.web.model.ParticipantModel;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class EventMapperTest {
 
@@ -15,26 +17,7 @@ class EventMapperTest {
     public static final String EVENT_NAME_WYDARZENIE = "Wydarzenie";
 
     @Test
-    void givenMapper_whenFrom_thenEntityEquals() {
-        //Given
-        EventMapper eventMapper = new EventMapper();
-        EventModel eventModel = new EventModel();
-        eventModel.setId(EVENT_ID_1);
-        eventModel.setName(EVENT_NAME_WYDARZENIE);
-
-        //When
-        EventEntity eventEntity = eventMapper.from(eventModel);
-
-        //Then
-        assertAll(
-                () -> assertNotNull(eventEntity, "EventEntity is null"),
-                () -> assertNotNull(eventEntity.getId(), "EventEntity.id is null"),
-                () -> assertNotNull(eventEntity.getName(), "EventEntity.name is null")
-        );
-    }
-
-    @Test
-    void givenMapper_whenFrom_thenModelEquals() {
+    void givenMapper_whenFromEntity_thenModelEquals() {
         //Given
         EventMapper eventMapper = new EventMapper();
         EventEntity eventEntity = new EventEntity();
@@ -53,7 +36,7 @@ class EventMapperTest {
     }
 
     @Test
-    void givenMapper_whenFrom_thenEntityEquals1() {
+    void givenMapper_whenFromModel_thenEntityEquals() {
         //Given
         EventMapper eventMapper = new EventMapper();
         EventModel eventModel = new EventModel();
@@ -74,5 +57,50 @@ class EventMapperTest {
         );
     }
 
+    @Test
+    void givenMapperAndDateTime_whenFromModel_thenEntityEquals() {
+        //Given
+        EventMapper eventMapper = new EventMapper();
+        EventModel eventModel = new EventModel();
+        ParticipantModel participantModel = new ParticipantModel();
+        LocalDateTime now = LocalDateTime.now();
+        eventModel.setId(EVENT_ID_1);
+        eventModel.setHost(participantModel);
+        eventModel.setDateTime(now);
 
+        //When
+        EventEntity eventEntity = eventMapper.from(eventModel);
+
+        //Then
+        assertAll(
+                () -> assertNotNull(eventEntity, "EventEntity is null"),
+                () -> assertNotNull(eventEntity.getId(), "EventEntity.id is null"),
+                () -> assertNotNull(eventEntity.getDateTime(), "EventEntity.dateTime is null"),
+                () -> assertNotNull(eventEntity.getHost(), "EventEntity.host is null")
+        );
+    }
+
+    @Test
+    void givenMapperAndDateTime_whenFromEntity_thenModelEquals() {
+        //Given
+        EventMapper eventMapper = new EventMapper();
+        EventEntity eventEntity = new EventEntity();
+        ParticipantEntity participantEntity = new ParticipantEntity();
+
+        LocalDateTime now = LocalDateTime.now();
+        eventEntity.setId(EVENT_ID_1);
+        eventEntity.setHost(participantEntity);
+        eventEntity.setDateTime(now);
+
+        //When
+        EventModel eventModel = eventMapper.from(eventEntity);
+
+        //Then
+        assertAll(
+                () -> assertNotNull(eventModel, "EventModel is null"),
+                () -> assertNotNull(eventModel.getId(), "EventModel.id is null"),
+                () -> assertNotNull(eventModel.getDateTime(), "EventModel.dateTime is null"),
+                () -> assertNotNull(eventModel.getHost(), "EventModel.host is null")
+        );
+    }
 }

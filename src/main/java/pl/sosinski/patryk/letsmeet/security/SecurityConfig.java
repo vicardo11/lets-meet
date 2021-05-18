14a6555232daf443b7dc69f24072043fddd7868f
Vicardo1 <vicardo95@gmail.com>
2021-05-18 13:07:28 +0200

    Add Spring Security and login form

---
+++ src/main/java/pl/sosinski/patryk/letsmeet/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package pl.sosinski.patryk.letsmeet.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT email, password, 'true' AS enabled FROM participants where email=?")
+                .authoritiesByUsernameQuery(
+                        "SELECT email, 'ROLE_USER' FROM participants where email=?")
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/events", true)
+                .permitAll();
+    }
+}
